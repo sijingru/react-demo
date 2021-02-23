896f55ff38642572c97eec9040658713e1710add
Sijingru <1433270020@qq.com>
2021-02-23 10:06:53 +0800

    feat(function) 完成todolist功能

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,17 +3,26 @@ import ReactDom from 'react-dom'
 class TodoList extends Component {
 
 	render() {
-		return <div>
-			{
-				this.props.todoList.map(item => (
-					(
+		const AllStatus = this.props.doneStatus,
+		showList = this.props.todoList.filter(item => item.status === AllStatus)
+		let list  = ''
+		if(AllStatus) {
+			list = showList.map(item => (
+						<section key={item.id}>
+							{/* <input type="checkbox" onChange={() => this.props.changeTaskStatus(item.id)} readOnly value={item.status} checked={item.status ? 'checked' : null}/> */}
+							<span>{item.name}</span>
+						</section>
+				))
+		} else {
+			list = showList.map(item => (
 						<section key={item.id}>
 							<input type="checkbox" onChange={() => this.props.changeTaskStatus(item.id)} readOnly value={item.status} checked={item.status ? 'checked' : null}/>
 							<span>{item.name}</span>
 						</section>
-					)
 				))
-			}
+		}
+		return <div>
+			{list}
 		</div>
 	}
 }
--- src/react.js
+++ src/react.js
@@ -8,28 +8,7 @@ import PutTask from './PutTask.jsx'
 class App extends Component {
 	constructor(props) {
 		super(props);
-    this.state = {oriTodoList: [
-				{
-					name: '待办1',
-					status: false,
-					id: 1
-				},
-				{
-					name: '待办2',
-					status: false,
-					id: 2
-				},
-				{
-					name: '待办3',
-					status: false,
-					id: 3
-				},
-				{
-					name: '待办4',
-					status: false,
-					id: 4
-				},
-			],
+    this.state = {
 			todoList: [
 				{
 					name: '待办1',
@@ -52,6 +31,7 @@ class App extends Component {
 					id: 4
 				},
 			],
+			doneStatus: false
 		};
 	}
 	changeTaskStatus (id)  {
@@ -63,8 +43,7 @@ class App extends Component {
 		this.setState({todoList})
 	}
 	changeShowList (flag)  {
-		const a = this.state.oriTodoList.filter(item => item.status === flag )
-		this.setState({todoList: a})
+		this.setState({doneStatus: flag})
 	}
 	addTask (taskName)   {
 		const todoList = this.state.todoList
@@ -75,16 +54,15 @@ class App extends Component {
 		})
 		this.setState({
 			todoList,
-			oriTodoList: todoList
 		})
 	}
 	render() {
 		return <div>
 				<div>
-					<span onClick={() => this.changeShowList(true)}>待办</span>
-					<span onClick={() => this.changeShowList(false)}>已完成</span>
+					<span onClick={() => this.changeShowList(false)}>待办</span>
+					<span onClick={() => this.changeShowList(true)}>已完成</span>
 				</div>
-				<TodoList todoList={this.state.todoList} changeTaskStatus={(id) => this.changeTaskStatus(id)} />
+				<TodoList todoList={this.state.todoList} doneStatus={this.state.doneStatus} changeTaskStatus={(id) => this.changeTaskStatus(id)} />
 				<PutTask addTask={(taskName) => this.addTask(taskName)}/>
 		</div>
 	}
