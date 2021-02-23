8db29ab7ad0fe72daeb680eea4c2ed2e2a4944ab
Sijingru <1433270020@qq.com>
2021-02-23 18:13:24 +0800

    feat(router) 新增路由功能

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
-import ReactDom from 'react-dom'
+import PutTask from './PutTask'
 class TodoList extends Component {
-
+	constructor(props) {
+		super(props)
+		this.state = {
+			todoList: [
+				{
+					name: '待办1',
+					status: false,
+					id: 1
+				},
+				{
+					name: '待办2',
+					status: false,
+					id: 2
+				},
+				{
+					name: '待办3',
+					status: false,
+					id: 3
+				},
+				{
+					name: '待办4',
+					status: false,
+					id: 4
+				},
+			],
+			doneStatus: false,
+		}
+	}
+	changeTaskStatus (id)  {
+		const todoList = this.state.todoList;
+		todoList.map(d => {
+			if (d.id === id) d.status = !d.status;
+			return d;
+		});
+		this.setState({todoList})
+	}
+	changeShowList (flag)  {
+		this.setState({doneStatus: flag})
+	}
+	addTask (taskName)   {
+		const todoList = this.state.todoList
+		todoList.push({
+			name: taskName,
+			status: false,
+			id: todoList.length + 1
+		})
+		this.setState({
+			todoList,
+		})
+	}
 	render() {
-		const AllStatus = this.props.doneStatus,
-		showList = this.props.todoList.filter(item => item.status === AllStatus)
+		const AllStatus = this.state.doneStatus,
+		showList = this.state.todoList.filter(item => item.status === AllStatus)
 		let list  = ''
 		if(AllStatus) {
 			list = showList.map(item => (
@@ -16,13 +65,18 @@ class TodoList extends Component {
 		} else {
 			list = showList.map(item => (
 						<section key={item.id}>
-							<input type="checkbox" onChange={() => this.props.changeTaskStatus(item.id)} readOnly value={item.status} checked={item.status ? 'checked' : null}/>
+							<input type="checkbox" onChange={() => this.changeTaskStatus(item.id)} readOnly value={item.status} checked={item.status ? 'checked' : null}/>
 							<span>{item.name}</span>
 						</section>
 				))
 		}
 		return <div>
+			<div>
+				<span onClick={() => this.changeShowList(false)}>待办</span>
+				<span onClick={() => this.changeShowList(true)}>已完成</span>
+			</div>
 			{list}
+			<PutTask addTask={(taskName) => this.addTask(taskName)}/>
 		</div>
 	}
 }
--- src/react.js
+++ src/react.js
@@ -1,73 +1,37 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import './index.less'
-import TopBar from './TopBar.jsx'
 import TodoList from './TodoList.jsx'
-import PutTask from './PutTask.jsx'
 import NameForm from './NameForm.jsx'
 class App extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
-			todoList: [
-				{
-					name: '待办1',
-					status: false,
-					id: 1
-				},
-				{
-					name: '待办2',
-					status: false,
-					id: 2
-				},
-				{
-					name: '待办3',
-					status: false,
-					id: 3
-				},
-				{
-					name: '待办4',
-					status: false,
-					id: 4
-				},
-			],
-			doneStatus: false
+			routePath: [
+				'todoList',
+				'nameForm'
+			]
 		};
 	}
-	changeTaskStatus (id)  {
-		const todoList = this.state.todoList;
-		todoList.map(d => {
-			if (d.id === id) d.status = !d.status;
-			return d;
-		});
-		this.setState({todoList})
-	}
-	changeShowList (flag)  {
-		this.setState({doneStatus: flag})
-	}
-	addTask (taskName)   {
-		const todoList = this.state.todoList
-		todoList.push({
-			name: taskName,
-			status: false,
-			id: todoList.length + 1
-		})
-		this.setState({
-			todoList,
-		})
-	}
+	
 	render() {
 		return <div>
-				<div>
-					<span onClick={() => this.changeShowList(false)}>待办</span>
-					<span onClick={() => this.changeShowList(true)}>已完成</span>
-				</div>
-				<TodoList todoList={this.state.todoList} doneStatus={this.state.doneStatus} changeTaskStatus={(id) => this.changeTaskStatus(id)} />
-				<PutTask addTask={(taskName) => this.addTask(taskName)}/>
-				<NameForm ></NameForm>
+				{
+					this.state.routePath.map(item => {
+						return <button >
+								<Link to={item}>{item}</Link>
+							</button>
+					})
+				}
+				<Route exact path='/todoList' component={TodoList}></Route>
+				<Route exact path='/nameForm' component={NameForm}></Route>
 		</div>
 	}
 }
 
-ReactDom.render(<App />, document.getElementById('reactApp'))
+ReactDom.render((
+	<Router>
+		<Route path='/' component={App}></Route>
+	</Router>
+), document.getElementById('reactApp'))
